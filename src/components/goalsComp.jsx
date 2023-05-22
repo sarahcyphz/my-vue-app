cdf75dac368f3c87521621361fd81211d7d56574
sarahcyphz <sarah.cyphers@neudesic.com>
2023-05-22 09:14:57 -0400

    ...

---
+++ src/components/goalsComp.jsx
@@ -0,0 +1,10 @@
+export const Goals = [{
+    id: 0,
+    description: 'Experience an office environment',
+  }, {
+    id: 1,
+    description: 'Meet people and make connections',
+  }, {
+    id: 2,
+    description: 'Obtain a better understanding of how programming languages work/interact with eachother',
+  }];
---
+++ src/components/hobbiesComp.jsx
@@ -0,0 +1,19 @@
+export const Hobbies = [{
+    id: 0,
+    description: 'Crocheting',
+  }, {
+    id: 1,
+    description: 'Cooking',
+  }, {
+    id: 2,
+    description: 'Reading',
+  }, {
+    id: 3,
+    description: 'Hiking',
+  }, {
+    id: 4,
+    description: 'Traveling',
+  }, {
+    id: 5,
+    description: 'Movies',
+  }];
