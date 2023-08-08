d86f52363585a75d5d7626ec93fed690c2ade5c7
sarahcyphz <sarah.cyphers@neudesic.com>
2023-08-08 13:41:46 -0400

    cleaning up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import Hobbies from "./components/Hobbies.jsx";
-import Goals from "./components/Goals.jsx";
 import Buttons from "./Buttons.jsx";
-import Paintings from "./components/ChicagoArt";
-import frame from "./images/frame.png";
 import { useState } from "react";
 import "./App.css";
 
@@ -10,10 +6,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Sarah Cyphers</h1>
-      <h4>Sarah.Cyphers@neudesic.com</h4>
       <div className="grid-container">
-        <Buttons />
+        <div className="grid-child contact"></div>
         <div className="grid-child aboutMe">
           <h2 className="h2">About Me</h2>
           <p>
@@ -21,14 +15,6 @@ function App() {
             currently enrolled at The Ohio State University pursuing a degree in
             information systems with a minor in textiles and CIS. .
           </p>
-        </div>
-        <Goals />
-        <Hobbies />
-      </div>
-      <div className="grid-container">
-        <Paintings />
-        <div className="grid-child frame">
-          <img src={frame} width={400} height={295} alt="frame" />
         </div>
       </div>
     </>
--- src/specialButton.js
+++
@@ -1,9 +0,0 @@
-
-
-export const funImage = [{
-    name: 'blank',
-    pic: '',
-}, {
-    name: 'fesh',
-    pic: './fish.jpeg',
-}];
